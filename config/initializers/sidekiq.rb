8e2bb8dd588292fb92cf96954a828a78fa3755f9
yuriadams <yuriadams@gmail.com>
2017-03-01 23:01:19 +0000

    missing config file

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,8 @@
+REDIS_URL = 'redis://localhost:6379/1'
+Sidekiq.configure_server do |config|
+  config.redis = { url: REDIS_URL }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: REDIS_URL }
+end

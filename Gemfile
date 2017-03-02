0288052369cb9abd089abceb7ae2640052b01b3f
Yuri Adams <yuriadams@Yuris-MacBook-Pro.local>
2017-03-02 12:00:38 +0000

    test lobby controller

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   %w[rspec-core rspec-expectations rspec-mocks rspec-rails rspec-support].each do |lib|
     gem lib, :git => "https://github.com/rspec/#{lib}.git", :branch => 'master'
   end
+  gem 'rails-controller-testing'
 end
 
 group :development do
---
+++ spec/controllers/lobby_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe LobbyController do
+  let!(:dialect){ Dialect.create(slug: "yoda") }
+  let!(:room){ Room.create(name: "General") }
+  let!(:user){ User.create(email: "yuriadams@gmail.com", dialect: dialect) }
+  let(:text){ "Message xxxxxx" }
+  let!(:message_1){ Message.create(text: text, user: user, room: room, created_at: DateTime.new(2017,3,2,0,4,0)) }
+  let!(:message_2){ Message.create(text: text, user: user, room: room, created_at: DateTime.new(2017,3,2,0,5,0)) }
+  let!(:message_3){ Message.create(text: text, user: user, room: room, created_at: DateTime.new(2017,3,2,0,6,0)) }
+
+  before do
+    session[:user_id] = user.id
+  end
+
+  subject{ get :index, params: { room_id: room.id } }
+
+  describe "GET index" do
+    it " renders index page" do
+      subject
+      expect(response).to render_template(:index)
+    end
+
+    it " returns all rooms" do
+      subject
+      expect(assigns(:rooms)).to eq([room])
+    end
+
+    it " returns the messages from a specific room" do
+      subject
+      expect(assigns(:messages).map{|m| [m.sender, m.time, m.text]}).to match_array([["yuriadams@gmail.com", "02/03/2017 00:04", "Message xxxxxx"],
+                                                                                     ["yuriadams@gmail.com", "02/03/2017 00:05", "Message xxxxxx"],
+                                                                                     ["yuriadams@gmail.com", "02/03/2017 00:06", "Message xxxxxx"]])
+    end
+  end
+end
